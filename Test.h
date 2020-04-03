9e2ba7b3435c6ec9dc73aca34b00fd26a62590aa
willlead <willlead@naver.com>
2020-04-03 17:10:39 +0900

    Create Test.h
    test header

---
+++ Test.h
@@ -0,0 +1,14 @@
+#import <Foundation.h>
+
+@interface MyFirstClass: NSObject
+{
+  int myFirstInt;
+  NSString* myName;
+  NSString* mySecret;
+}
+
+- (void) myFristMehtod;
+
++ (void)itIsClassMethod;
+
+@end
